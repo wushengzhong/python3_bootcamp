91b13509c166175d511a70389cf5be8abbd6065a
Wenyi Yan <Wenyi@Wenyis-MacBook-Pro-2.local>
2018-04-28 15:51:59 -0700

    string example from Python3 bootcamp

---
+++ python_pilot.py
@@ -0,0 +1,32 @@
+# string
+# just retun the value
+'hello'
+'this is also a string'
+# only the last result was displayed
+
+# print() method return all value
+print('hello')
+print('this is also a string')
+# screen result hello rather than 'hello'
+
+
+# escape sequence
+print('hello\nwrold')
+print("I'm a double-quotted string here")
+
+# tab escape sequence
+print("hello\ttab")
+
+# indexing
+len('hello')
+len("I'm hungry")
+
+mystring="Hello World"
+mystring
+
+mystring[0]
+mystring[8]
+
+mystring[-2]==mystring[9]
+
+mystring='abcdefghijk'
