152dee7d7aaf0b80c2b14878602aae5850a510ee
System Administrator <root@Wenyis-MacBook-Pro-2.local>
2018-04-28 20:47:50 -0700

    update example

--- myexample.py
+++ myexample.py
@@ -9,13 +9,16 @@ def twoSums(lst,target):
 print(twoSums([2,3,5,9,7,8],11))
 
 
-sets([1,2,3,4,5,4,3,5
+sets([1,2,3,4,5,4,3,5])
 
 knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 
 questions = ['name', 'quest', 'favorite color']
 answers = ['lancelot', 'the holy grail', 'blue']
 
-zip(questions,answers)
+for k,v in zip(questions,answers):
+	print("The %s is %s" %(k,v))
 
+name = raw_input("who are you?")
+print "hello %s" % (name,)
 
